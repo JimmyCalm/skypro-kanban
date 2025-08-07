45d96080cd52777ce0afe15661bafab4e44ad122
Timur Udovenko <timur96udovenko@gmail.com>
2025-08-07 11:21:17 +0300

    исправил проблему с модалками

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import Header from './Header/Header';
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "./Header/Header";
+import PopBrowse from "./Popups/PopBrowse";
+import PopNewCard from "./Popups/PopNewCard";
 
 function Layout() {
   const location = useLocation();
-  const hideHeader = location.pathname === '/login' || location.pathname === '/register';
+  const hideHeader =
+    location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <div className="wrapper">
       {!hideHeader && <Header />}
+      <PopBrowse />
+      <PopNewCard />
       <Outlet />
     </div>
   );
